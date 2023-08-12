7429c4cd49e4f187802a4eb5c89980cc413d941e
rsunar <tech.channel.002@gmail.com>
2023-08-12 21:09:57 +0530

    finishing object and its weird behaviour

---
+++ 02Basics/objects.ts
@@ -0,0 +1,19 @@
+const User = {
+    name : 'Roshan',
+    email:'email@email.com',
+    isActive : true 
+}
+
+function createUser ({name:string,isPaid:boolean = false}){
+
+}
+
+
+createUser({name:'roshan',isPaid:false})
+
+function createCourse ():{name:string,price:number}{
+    return{ name:'NextJS',price : 399}
+}
+
+
+export {}
