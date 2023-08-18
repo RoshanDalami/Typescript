f5fc3f4b3d1c931e84d7dfc17e400e2ab53fcfc3
rsunar <tech.channel.002@gmail.com>
2023-08-18 10:07:51 +0545

    indepth javascript

---
+++ leet.js
@@ -0,0 +1,22 @@
+// const user = {
+//   username: "roshan",
+//   price: 999,
+//   welcomeMessage: function () {
+//     console.log(`${this.username}, welcome to website`);
+//     console.log(this);
+//   },
+// };
+
+// // user.welcomeMessage();
+// // user.username = "Dalami";
+// // user.welcomeMessage();
+
+// console.log(this);
+
+//arrow functions
+const chai = () => {
+  let username = "roshan";
+  console.log(this);
+};
+
+chai();
